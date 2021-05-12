162dbf17d0fb0ca8d31907c7ee3a8718fb3c7b41
Sowmya Moturu <sowmya_moturu@epam.com>
2021-05-12 18:20:57 +0530

    updated failed tests

--- server/src/test/java/com/example/TestGreeter.java
+++ server/src/test/java/com/example/TestGreeter.java
@@ -20,7 +20,7 @@ public class TestGreeter {
 	  public void greetShouldIncludeTheOneBeingGreeted() {
 		 System.out.println(Objects.isNull(greeter));
 	    String someone = "World";
-	    Assert.assertTrue(greeter.greet(someone).contains(someone + "sometext"));
+	    Assert.assertTrue(greeter.greet(someone).contains(someone));
 	  }
 	 
 	 @Test(groups = {"regression"})
