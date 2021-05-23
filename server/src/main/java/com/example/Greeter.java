95e5d58d7f4deeb0295961a5093af1b651976ac4
Sowmya Moturu <sowmya_moturu@epam.com>
2021-05-23 17:01:15 +0530

    Inserted issues to test Sonar

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -5,15 +5,35 @@ package com.example;
  */
 public class Greeter {
 
-  /**
-   * This is a constructor.
-   */
-  public Greeter() {
+	/**
+	 * This is a constructor.
+	 */
+	public Greeter() {
 
-  }
+	}
 
-  //TODO: Add javadoc comment
-  public String greet(String someone) {
-    return String.format("Hello How are you, %s!", someone);
-  }
+	// TODO: Add javadoc comment
+	public String greet(String someone) {
+		return String.format("Hello How are you, %s!", someone);
+	}
+
+	public String greet1(String someone) {
+		return String.format("Hello How are you, %s!", someone);
+	}
+	
+	public  void myMethod() {
+		String unusedVariable;
+		int val;
+		if(true) {
+			
+		}
+		else {
+			val = 20;
+		}
+		boolean myflag;
+		if(myflag=true) {
+			val = 5;
+		}
+		
+	}
 }
